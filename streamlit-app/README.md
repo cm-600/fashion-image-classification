26ab632bb07e990b566ca1ea31791098222275ca
cm-600 <carolyn.morris.600@gmail.com>
2024-09-07 15:41:22 -0700

    Add streamlit app

---
+++ streamlit-app/README.md
@@ -0,0 +1,12 @@
+---
+title: Fashion Image Classification
+emoji: 🏢
+colorFrom: purple
+colorTo: red
+sdk: streamlit
+sdk_version: 1.38.0
+app_file: app.py
+pinned: false
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ streamlit-app/app.py
@@ -0,0 +1,28 @@
+import os
+import streamlit as st
+from huggingface_hub import login
+from transformers import pipeline
+from PIL import Image
+
+hf_token = st.secrets["HUGGINGFACE_TOKEN"]
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = hf_token
+
+login(token=hf_token)
+
+pipeline = pipeline(task="image-classification", model="cm600/fashion_classifier")
+
+st.title("Fashion Image Classification")
+
+file_name = st.file_uploader("Upload a fashion image to view its top 5 predicted probabilities for each of the following categories: \
+                              T-shirt/top, Trouser, Pullover, Dress, Coat, Sandal, Shirt, Sneaker, Bag, Ankle boot.")
+
+if file_name is not None:
+    col1, col2 = st.columns(2)
+
+    image = Image.open(file_name)
+    col1.image(image, use_column_width=True)
+    predictions = pipeline(image)
+
+    col2.header("Probabilities")
+    for p in predictions:
+        col2.subheader("{}: {}%".format(p['label'], round(p['score'] * 100, 1)))
